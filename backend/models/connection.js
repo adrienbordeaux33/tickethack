a436c67b7dbb40ba69e29ea0f4e94f0854cb2d90
Adrien <adrien.bordeaux33@outlook.com>
2025-07-01 16:14:49 +0200

    16H15 fonctionnel

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 // Insert your connection string inside this variable
-const connectionString = 'mongodb+srv://guimeyer64:ltGcDQzb95gqs7bD@cluster0.p63mzk5.mongodb.net/tickethack';
+const connectionString =
+  "mongodb+srv://adrienbordeaux33:yEHpz0gqNxcQeCAi@cluster0.y07cmvj.mongodb.net/tickethack";
 
 mongoose.set("strictQuery", true); // Remove Mongoose warning in console
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))
-  .catch(error => console.error(error));
-
+  .catch((error) => console.error(error));
 
 module.exports = connectionString; // Do not edit/remove this line
